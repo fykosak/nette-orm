588d9568241baa16b3adea05aa4f576f508cd1b9
Michal Mišo Červeňák <miso@fykos.cz>
2021-07-29 00:17:07 +0200

    Service and model can be ommited

--- src/ORMExtension.php
+++ src/ORMExtension.php
@@ -20,16 +20,15 @@ class ORMExtension extends CompilerExtension
 
     final protected function registerORMService(string $tableName, array $fieldDefinitions): void
     {
-        $serviceClassName = $fieldDefinitions['serviceClassName'] ?? ($fieldDefinitions['service'] ?? null);
-        $modelClassName = $fieldDefinitions['modelClassName'] ?? ($fieldDefinitions['model'] ?? null);
-        if ($serviceClassName) {
-            $builder = $this->getContainerBuilder();
-            $factory = $builder->addDefinition($this->prefix($tableName . '.service'));
-            if (isset($fieldDefinitions['context'])) {
-                $factory->setFactory($serviceClassName, [$tableName, $modelClassName, $fieldDefinitions['context']]);
-            } else {
-                $factory->setFactory($serviceClassName, [$tableName, $modelClassName]);
-            }
+        $serviceClassName = $fieldDefinitions['serviceClassName'] ?? ($fieldDefinitions['service'] ?? DummyService::class);
+        $modelClassName = $fieldDefinitions['modelClassName'] ?? ($fieldDefinitions['model'] ?? DummyModel::class);
+
+        $builder = $this->getContainerBuilder();
+        $factory = $builder->addDefinition($this->prefix($tableName . '.service'));
+        if (isset($fieldDefinitions['context'])) {
+            $factory->setFactory($serviceClassName, [$tableName, $modelClassName, $fieldDefinitions['context']]);
+        } else {
+            $factory->setFactory($serviceClassName, [$tableName, $modelClassName]);
         }
     }
 }
